833d77961c7b450aa18494e9dad6061d97ba2328
Laura Tonning <tonning@gmail.com>
2024-07-21 20:06:12 +0200

    Add API versions

---
+++ src/Enums/ApiVersion.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Zzz\ShopifyGraphql\Enums;
+
+enum ApiVersion: string
+{
+    case VERSION_2024_07 = '2024-07';
+    case VERSION_2024_04 = '2024-04';
+    case VERSION_2024_01 = '2024-01';
+    case VERSION_2023_10 = '2023-10';
+    case VERSION_2023_07 = '2023-07';
+}
