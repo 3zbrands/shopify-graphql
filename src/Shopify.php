56cfd160e9bf2a8231b10ed1283b870895fef197
Laura Tonning <tonning@gmail.com>
2024-07-21 20:06:38 +0200

    Allow for both storefront and admin APIs

--- src/Shopify.php
+++ src/Shopify.php
@@ -2,28 +2,56 @@
 
 namespace Zzz\ShopifyGraphql;
 
+use Exception;
 use Saloon\Http\Connector;
 use Zzz\ShopifyGraphql\Resources\Cart;
-use Saloon\Http\Auth\TokenAuthenticator;
+use Zzz\ShopifyGraphql\Enums\ApiVersion;
 use Saloon\Http\Auth\HeaderAuthenticator;
+use Zzz\ShopifyGraphql\Resources\Products;
 
 class Shopify extends Connector
 {
-    private string $apiVersion;
+    private string $baseUrl = '';
+    private HeaderAuthenticator|null $headerAuthenticator = null;
 
-    public function __construct(public readonly string $storeId, public string $apiToken)
+    public function __construct(
+        public readonly string $storeId,
+        public readonly string $adminAccessToken,
+        public readonly string $storefrontAccessToken,
+        public readonly ApiVersion $apiVersion
+    )
     {
-        $this->apiVersion = '2024-04';
+        //
     }
 
     protected function defaultAuth(): HeaderAuthenticator
     {
-        return new HeaderAuthenticator($this->apiToken, 'X-Shopify-Storefront-Access-Token');
+        throw_if(blank($this->headerAuthenticator), Exception::class, 'Must specify which API to connect to.');
+
+        return $this->headerAuthenticator;
     }
 
     public function resolveBaseUrl(): string
     {
-        return "https://{$this->storeId}.myshopify.com/api/{$this->apiVersion}/graphql.json";
+        throw_if(blank($this->baseUrl), Exception::class, 'Must specify which API to connect to.');
+
+        return $this->baseUrl;
+    }
+
+    protected function connectToStorefront(): static
+    {
+        $this->baseUrl = "https://{$this->storeId}.myshopify.com/api/{$this->apiVersion->value}/graphql.json";
+        $this->headerAuthenticator = new HeaderAuthenticator($this->storefrontAccessToken, 'X-Shopify-Storefront-Access-Token');
+
+        return $this;
+    }
+
+    protected function connectToAdmin(): static
+    {
+        $this->baseUrl = "https://{$this->storeId}.myshopify.com/admin/api/{$this->apiVersion->value}/graphql.json";
+        $this->headerAuthenticator = new HeaderAuthenticator($this->adminAccessToken, 'X-Shopify-Access-Token');
+
+        return $this;
     }
 
     protected function defaultHeaders(): array
@@ -36,6 +64,15 @@ class Shopify extends Connector
 
     public function cart(): Cart
     {
+        $this->connectToStorefront();
+
         return new Cart($this);
+    }
+
+    public function products(): Products
+    {
+        $this->connectToAdmin();
+
+        return new Products($this);
     }
 }
